d3602166698a070ce8c036bec45984e143fea417
Retta-skull <burutal@naver.com>
2021-06-10 23:11:52 +0900

    d

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
+
 class Board {
     grid;
     reset() {
@@ -19,7 +20,34 @@ class Board {
         {length: ROWS}, () => Array(COLS).fill(0)
       );
     }
-  }
+  }//보드 초기화
+
+
+class Piece {
+    x;
+    y;
+    color;
+    shape;
+    ctx;
+    
+    constructor(ctx) {
+      this.ctx = ctx;
+      this.spawn();
+    }
+    
+    spawn() {
+      this.color = 'blue';
+      this.shape = [
+        [2, 0, 0], 
+        [2, 2, 2], 
+        [0, 0, 0]
+      ];
+      
+      // Starting position.
+      this.x = 3;
+      this.y = 0;
+    }
+}// 테트리스 조각 모음
 
 let board = new Board();
 function play() {
